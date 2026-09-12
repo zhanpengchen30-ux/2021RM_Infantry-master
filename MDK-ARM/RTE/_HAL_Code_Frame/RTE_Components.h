99055af50d35aec177ca4e16aed0a567644e4485
zhanpengchen30-ux <zhanpengchen30@gmail.com>
2026-09-12 12:59:38 +0800

    Initial commit

---
+++ MDK-ARM/RTE/_HAL_Code_Frame/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HAL_Code_Frame' 
+ * Target:  'HAL_Code_Frame' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
